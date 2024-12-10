13eed482fe82d7cb8d2545b88d1571d0fcca2f5e
joanalnu <joanalnu@outlook.com>
2024-12-10 18:37:26 +0100

    sync updates

--- README.md
+++ README.md
@@ -4,6 +4,55 @@ Computing surface temperature heatmaps using LandSat-8 publicly available images
 [![repo](https://img.shields.io/badge/GitHub-thermal_analysis-blue.svg?style=flat)](https://github.com/joanalnu/thermal_analysis)
 ![Python Version](https://img.shields.io/badge/Python-3.9%2B-blue)
 
+
+# Installation
+To use the code in this repository you can clone it running the following command in your shell.
+```bash
+git clone https://github.com/joanalnu/thermal_analysis.git
+```
+Next, you can install it as a library using pip.
+```bash
+pip install thermal_analysis
+```
+Then you can import the code anywhere like
+```python
+import thermal_analysis as ta
+```
+
+# **Function Descriptions**
+
+## **`compute_surface_temperature`**
+- **Description:** Computes the surface temperature from a LandSat-8 satellite image.
+- **Arguments:**
+  - `file_path` (required): Path to a `.TIF` file containing the satellite image.
+  - **Optional parameters:**
+    - `degree`: Unit of temperature. Can be one of:
+      - `'Celsius'` (default)
+      - `'Kelvin'`
+      - `'Fahrenheit'`
+- **Returns:** A tuple composed by:
+  - `metaData`: a list containing file name information and temperature unit.
+  - `temperatureData`: a 2D array with the temperature values.
+
+## **`generate_heatmap`**
+- **Description:** Creates a heatmap to visualize temperature data using a color map.
+- **Arguments:**
+  - `data` (required): A NumPy array containing temperature values.
+    - `colormap`: Specifies the color map to use (e.g., `'viridis'`, `'plasma'`, `'inferno'`, see [matplotlib](https://matplotlib.org/stable/users/explain/colors/colormaps.html)). Defaults to `'plasma'`.
+    - `output_format`: Desired file format for the heatmap (e.g., `'png'`, `'jpeg', 'jpg', 'pdf'`). Defaults to `'png'`.
+    - `title`: Title to display on the heatmap. Defaults to `LandSat-8 Heatmap`.
+    - `label`: Label for the color bar. Defaults to `'Temperature (<unit>)'`.
+    - `dpi`: Resolution of the output image. Defaults to `300`.
+    - `grid`: Boolean to toggle gridlines. Defaults to `False`.
+    - `custom_range`: Tuple specifying the (min, max) range of temperatures. Defaults to auto-scaling.
+    - `histo`: Boolean to toggle a histogram to the side of the map. Defaults to `True`.
+      - `histo_color`: Color of the histogram columns. Defaults to `orange`.
+      - `histo_grid`: Boolean to toggle gridlines. Defaults to `True`
+      - `histo_title`: Desired title for the histogram. Defaults to `'Distribution'`
+      - `histo_bins`: Desired size of temperature bins for histogram. Defaults to `0.1`.
+- **Returns:** Saves the heatmap as an image file.
+
+
 # Copyright
 #### **This repository is protected under the default copyright laws, meaning that the repository owner retains all the rights to this source code and no one may reproduce, distribute, or create derivative works from this work. [GitHub Licensing Notice](https://docs.github.com/en/repositories/managing-your-repositorys-settings-and-features/customizing-your-repository/licensing-a-repository)**
 
--- landsat8_heatmap.py
+++ landsat8_heatmap.py
@@ -6,58 +6,79 @@ from osgeo import gdal
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
+import thermal_analysis as ta
 
+def compute_surface_temperature(file_path, degree='celcius'):
+    # Reads raw data and compute surface temperature
 
-
-
-def  compute_surface_temperature(file_path):
     # Reading data
     ds = gdal.Open(file_path)
     data = ds.ReadAsArray()
 
-    # Create an empty array for temperatures
-    rows, cols = data.shape
-    temperatureData = np.zeros((rows, cols))
+    rows, cols = data.shape # read data shape
+    temperatureData = np.zeros((rows, cols)) # create empty array
 
     # Iterate through each pixel in the image
     for i in range(rows):
         for j in range(cols):
-            # Compute and store temp values
-            if data[i,j] == 0:
+            # Compute and store temp value
+            if data[i,j] == 0: # fast-forwading zeros to avoid division by 0
                 newVal = 0
             else:
-                newVal = 0.00341802* data[i,j] + 149.0 # Kelvin
-                newVal -= 273.15  # to Celsius
+                newVal = 0.00341802* data[i,j] + 149.0 # Kelvin (following LandSat-8 calibration docs)
+                if degree in ['celcius', 'Celcius', 'CELCIUS']:
+                    newVal -= 273.15 # to Celcius
+                if degree in ['Fahrenheit', 'fahrenheit', 'FAHRENHEIT']:
+                    newVal = (newVal -273.15) * 1.8 + 32 # to Fahrenheit
             temperatureData[i,j] = newVal
 
-    # The Array Way
+    # alternative array way (which does not account for zeros)
     # temperature_K = data*0.00341802+149.0
     # temperatureData = temperature_K-273.15
 
-    # Create a figure with gridspec to place heatmap and histogram side by side
+    metaData = [file_path, degree]
+    return (metaData, temperatureData)
+
+def generate_histogram(data, bins, color='orange', grid=True, title='Distribution', xlabel='Frequency'):
+    # Generates a histogram for the given data.
+    ax = plt.gca()  # Get the current axis
+    ax.hist(data[data > 0].ravel(), bins=bins, orientation='horizontal', color=color)
+    ax.set_title(title)
+    ax.set_xlabel(xlabel)
+    ax.grid(grid)
+
+def generate_heatmap(data, file_path, dpi=300, cmap='plasma', title='LandaSat-8 Heatmap', grid=False, custom_range=(None, None),
+                     histo=True, histo_color='orange', histo_grid=True, histo_title='Distribution', histo_bins=0.1):
+
+    # Creates a matplotlib figure mapping surface temperature and optionally an accompanying histogram.
+
     fig = plt.figure(figsize=(10, 6))
-    gs = gridspec.GridSpec(1, 2, width_ratios=[4, 1])  # 4:1 ratio for heatmap and histogram
+    if histo:
+        gs = gridspec.GridSpec(1, 2, width_ratios=[4, 1])  # 4:1 ratio for heatmap and histogram
+        ax0 = plt.subplot(gs[0])
+    else:
+        ax0 = plt.subplot()
 
     # Plot heatmap in the first subplot
-    ax0 = plt.subplot(gs[0])
-    heatmap = ax0.imshow(temperatureData, cmap='plasma')
+    vmin, vmax = custom_range
+    heatmap = ax0.imshow(data, cmap=cmap, vmin=vmin, vmax=vmax)
     plt.colorbar(heatmap, ax=ax0, label='Temperature ($^\circ C$)')
-    ax0.set_title('LandSat-8 Heatmap')
+    ax0.set_title(title)
+    ax0.grid(grid)
 
-    # Plot histogram in the second subplot
-    ax1 = plt.subplot(gs[1])
-    ax1.hist(temperatureData[temperatureData > 0].ravel(), bins=np.arange(np.min(temperatureData), np.max(temperatureData)+0.1, 0.1), orientation='horizontal', color='orange')
-    ax1.set_title('Distribution')
-    ax1.set_xlabel('Frequency')
-    # ax1.set_xscale('log')
-    ax1.grid(True)
+    if histo:
+        # Plot histogram in the second subplot
+        ax1 = plt.subplot(gs[1])
+        plt.sca(ax1)  # Set the second subplot as the current axis
+        bins = np.arange(np.min(data), np.max(data) + 0.1, histo_bins) # compute bins for histogram
+        ta.generate_histogram(data, bins, color=histo_color, grid=histo_grid, title=histo_title)
 
-    # Save the figure with the heatmap and histogram
+    # Save the figure
+    file_name = f'tempFig_{file_path.replace("/", "-").replace(".", "")}'
     plt.tight_layout()
-    plt.savefig(f'tempFig_{file_path.replace("/", "-").replace(".", "")}')
-    print(f'Saved as\ttempFig_{file_path.replace("/", "-").replace(".", "")}')
+    plt.savefig(file_name, dpi=dpi)
+    print(f'Saved as\t{file_name}')
     plt.show()
-
 
 # SINGLE FILE
 file_path = './' + str(input('File path: '))
